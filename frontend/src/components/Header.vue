95e5cbc928b51e4cd0dc23fa5a43eb0b667fc96f
Ignacio Illanes <ignacioillanes@Ignacios-MacBook-Pro.local>
2022-04-28 10:38:57 -0400

    Add header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-toolbar flat>
+      <v-toolbar-title>Periodico La Paz</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-item>
+        <v-btn text>
+          <router-link to="/">Destacados</router-link>
+        </v-btn>
+
+        <v-btn text>
+          <router-link to="/galeria">servicios</router-link>
+        </v-btn>
+
+        <v-btn text>
+          <router-link to="/requerimiento">Clasificados</router-link>
+        </v-btn>
+
+        <v-btn text>
+          <router-link to="/contacto">Contáctanos</router-link>
+        </v-btn>
+      </v-toolbar-item>
+    </v-toolbar>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPrincipal',
+
+  data: () => ({})
+}
+</script>
