efe4d1b836d7531e20723ef1fb49c8182303e06a
Ignacio Agramont <ignacio.agramont.11@gmail.com>
2022-04-29 00:25:26 -0400

    Final Cards

--- frontend/src/components/FormComponent.vue
+++ frontend/src/components/FormComponent.vue
@@ -1,75 +1,69 @@
-script<template>
+<template>
   <v-row justify="center">
-    <v-col cols="12" sm="10" md="8" lg="6">
-      <v-card ref="form">
-        <v-card-text>
-          <v-text-field
-            ref="name"
-            v-model="name"
-            :rules="[() => !!name || 'Campo obligatorio']"
-            :error-messages="errorMessages"
-            label="Nombre Completo"
-            placeholder="Ignacio Agramont"
-            required
-          ></v-text-field>
-          <v-text-field
-            ref="email"
-            v-model="email"
-            :rules="[
-              () => !!email || 'Campo obligatorio',
-            ]"
-            label="Email"
-            placeholder="email@email.com"
-            required
-          ></v-text-field>
-          <v-text-field
-            ref="phone"
-            v-model="phone"
-            :rules="[() => !!phone || 'Campo Obligatorio']"
-            label="Celular"
-            required
-            placeholder="77788899"
-          ></v-text-field>
-          <v-text-field
-            ref="subject"
-            v-model="subject"
-            :rules="[() => !!subject || 'Campo Obligatorio']"
-            label="Asunto"
-            required
-            placeholder="Asunto de la solicitud"
-          ></v-text-field>
-          <v-textarea 
-            ref="message"
-            v-model="message" 
-            :rules="[() => !!message || 'Campo Obligatorio']"
-            label="Mensaje"
-            required
-            placeholder="Mensaje de la solicitud"
-            >
-          </v-textarea>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn text> Cancel </v-btn>
-          <v-spacer></v-spacer>
-          <v-slide-x-reverse-transition>
-            <v-tooltip v-if="formHasErrors" left>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  icon
-                  class="my-0"
-                  v-bind="attrs"
-                  @click="resetForm"
-                  v-on="on"
-                >
-                  <v-icon>mdi-refresh</v-icon>
-                </v-btn>
-              </template>
-              <span>Refresh form</span>
-            </v-tooltip>
-          </v-slide-x-reverse-transition>
-          <v-btn color="primary" text @click="submit"> Submit </v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-col cols="12" sm="10" md="8" lg="6" ref="form">
+      <v-text-field
+        ref="name"
+        v-model="name"
+        :rules="[() => !!name || 'Campo obligatorio']"
+        :error-messages="errorMessages"
+        label="Nombre Completo"
+        placeholder="Ignacio Agramont"
+        required
+      ></v-text-field>
+      <v-text-field
+        ref="email"
+        v-model="email"
+        :rules="[() => !!email || 'Campo obligatorio']"
+        label="Email"
+        placeholder="email@email.com"
+        required
+      ></v-text-field>
+      <v-text-field
+        ref="phone"
+        v-model="phone"
+        :rules="[() => !!phone || 'Campo Obligatorio']"
+        label="Celular"
+        required
+        placeholder="77788899"
+      ></v-text-field>
+      <v-text-field
+        ref="subject"
+        v-model="subject"
+        :rules="[() => !!subject || 'Campo Obligatorio']"
+        label="Asunto"
+        required
+        placeholder="Asunto de la solicitud"
+      ></v-text-field>
+      <v-textarea
+        ref="message"
+        v-model="message"
+        :rules="[() => !!message || 'Campo Obligatorio']"
+        label="Mensaje"
+        required
+        placeholder="Mensaje de la solicitud"
+      >
+      </v-textarea>
+      <v-card-actions>
+        <v-btn text> Cancel </v-btn>
+        <v-spacer></v-spacer>
+        <v-slide-x-reverse-transition>
+          <v-tooltip v-if="formHasErrors" left>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                icon
+                class="my-0"
+                v-bind="attrs"
+                @click="resetForm"
+                v-on="on"
+              >
+                <v-icon>mdi-refresh</v-icon>
+              </v-btn>
+            </template>
+            <span>Refresh form</span>
+          </v-tooltip>
+        </v-slide-x-reverse-transition>
+        <v-btn color="primary" text @click="submit"> Submit </v-btn>
+      </v-card-actions>
     </v-col>
   </v-row>
 </template>
---
+++ frontend/src/components/MediaComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card class="mx-auto background-secondary" max-width="400">
+    <v-img
+      class="white--text align-end"
+      height="150px"
+      :src="require(`@/assets/img/img6.jpg`)"
+    >
+      <v-card-title class="text-black centermid"
+        >Más sobre nosotros</v-card-title
+      >
+    </v-img>
+
+    <v-card-text class="text-white">
+      <div>
+        Periodico "La Paz" es un medio de comunicación digital boliviano de
+        ámbito nacional e internacional y temática generalista, nacido en
+        noviembre de 2003 por iniciativa de su actual director.
+      </div>
+      <div>
+        Estamos presentes en redes sociales como Facebook, Twitter y Linkedin.
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
